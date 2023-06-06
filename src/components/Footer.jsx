886b8bcfc4abf5e6d553d15cbd7e054332138a78
JustInTimez <just.oost@gmail.com>
2023-06-06 17:37:22 +0200

    Adds favicon to site

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-info">© 2023 Capstone Podcast Project.</p>
+      <p className="footer-extra"><a href="https://www.flaticon.com/free-icons/bass" title="bass icons">Bass icons created by Good Ware - Flaticon</a></p>
+    </footer>
+  )
+}
